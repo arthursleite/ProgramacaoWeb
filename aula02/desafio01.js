96327b642849b1e4ffc2cd91b831017c7a6fb844
Arthur Leite <arthurmais4@gmail.com>
2023-09-04 17:34:47 -0300

    adicionando correcao desafios aula02

--- aula02/desafio01.js
+++ aula02/desafio01.js
@@ -1,18 +1,31 @@
+// Desafio 01 - Matriz Transposta
+
 let matrizA = [
     [1, 2],
     [3, 4],
     [5, 6]
-];
+]
+let matrizT = new Array(matrizA[0].length)
+// let matrizT = [];
+
 console.log("Matriz A:")
 console.table(matrizA);
 
-let matrizT = [];
+//Solução corrigida
+for (let i = 0; i < matrizT.length; i++) {
+    matrizT[i] = new Array(matrizA.length)
+    for (let j = 0; j <matrizA.length; j++) {
+        matrizT[i][j] = matrizA[j][i]
+    }
+}
 
-for (let i = 0; i < matrizA[0].length; i++) {
+//Minha solução (funcional)
+/*for (let i = 0; i < matrizA[0].length; i++) {
     matrizT[i] = [];
     for (let j = 0; j < matrizA.length; j++) {
         matrizT[i][j] = matrizA[j][i];
     }
-}
+}*/
+
 console.log("Matriz T:")
 console.table(matrizT);
