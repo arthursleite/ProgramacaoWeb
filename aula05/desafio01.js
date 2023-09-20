759ec56dacb0d56c1862dc76ff99980c23343f14
arthursleite <arthurmais4@gmail.com>
2023-09-20 20:40:26 -0300

    desafio01 finalizado

--- aula05/desafio01.js
+++ aula05/desafio01.js
@@ -1,16 +1,23 @@
-function printNumber(n) {
-    let counter = 0
-    if (typeof n == "number" && counter < 5) {
-        console.log(n, "é um número válido")
-        n = prompt("Digite um número com vírgula: ")
-        printNumber(n)
-    } else if (typeof n != "number") {
-        console.log(n, "não é um número com vírgula")
-        n = prompt("Digite um número com vírgula: ")
-        printNumber(n)
+function getNumbers() {
+    let input
+    let numbers
+
+    input = prompt("Digite 5 números separados por vírgula: ")
+    numbers = input.split(",").map(Number)
+
+    if (numbers.length !== 5 || numbers.some(isNaN)) {
+        // numbers.forEach(element => {
+        //     if (isNaN(element)) {
+        //         temp = element
+        //         console.log(`${temp} não é um número válido`)
+        //     }
+        // })
+        console.log("Valore inválidos, digite novamente")
+        getNumbers()
+    } else {   
+        console.log(numbers)
     }
 }
 
 const prompt = require("prompt-sync")()
-let n = prompt("Digite um número com vírgula: ")
-printNumber(n)
+getNumbers()
