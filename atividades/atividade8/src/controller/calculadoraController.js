8ae4eab5688548430a7d6177c14b7f389bfcc714
arthursleite <arthurmais4@gmail.com>
2023-11-06 23:27:39 -0300

    atividade8 finalizada

--- atividades/atividade8/src/controller/calculadoraController.js
+++ atividades/atividade8/src/controller/calculadoraController.js
@@ -6,13 +6,33 @@ function indexCalculadora(req, res) {
 
 function calcular(req, res) {
     const numeros = {
-        n1: req.body.n1,
-        n2: req.body.n2
+        n1: parseFloat(req.body.n1),
+        n2: parseFloat(req.body.n2),
+        operador: req.body.operador
     };
-    console.log(numeros);
-    res.render("resultado.html", {numeros});
-    // res.send(`${n1} + ${n2} = ${calculadora.somar(n1, n2)}`); 
-    //no exercicio, retornar o valor renderizando o html
+
+    const operador = req.body.operador;
+    let resultado;
+    switch (operador) {
+        case "+":
+            resultado = calculadora.somar(numeros.n1, numeros.n2);
+            break;
+        case "-":
+            resultado = calculadora.subtrair(numeros.n1, numeros.n2);
+            break;
+        case "*":
+            resultado = calculadora.multiplicar(numeros.n1, numeros.n2);
+            break;
+        case "/":
+            resultado = calculadora.dividir(numeros.n1, numeros.n2);
+            break;
+        default:
+            resultado = "Operação inválida";
+            break;
+    }
+
+
+    res.render("resultado.html", { numeros, resultado });
 }
 
 module.exports = {
